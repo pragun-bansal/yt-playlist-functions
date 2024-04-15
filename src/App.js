ce3483335fc8cea1e0ebe1701d283945aeb0a01a
pragun-bansal <pragun.bansal@yahoo.in>
2024-04-16 02:45:51 +0530

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import PlaylistCalculator from './PlaylistCalculator';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className="App ">
+      <header className="App-header  bg-gradient-to-r from-indigo-400 to-cyan-400">
+      <ToastContainer
+position="top-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
         <PlaylistCalculator />
       </header>
     </div>
